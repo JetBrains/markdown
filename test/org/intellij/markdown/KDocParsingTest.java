65e7b16970fabe949411b0d8666395ad26d45ee1
Valentin Fondaratov <fondarat@gmail.com>
2014-12-16 23:28:42 +0300

    one more test

--- test/org/intellij/markdown/KDocParsingTest.java
+++ test/org/intellij/markdown/KDocParsingTest.java
@@ -17,6 +17,24 @@ public class KDocParsingTest extends BaseParsingTest {
         defaultTest();
     }
 
+    public void testSmall() {
+        assertEquals(
+                "Markdown:MARKDOWN_FILE\n" +
+                "  Markdown:SECTION\n" +
+                "    Markdown:PARAGRAPH\n" +
+                "      Markdown:TEXT('Summary')\n" +
+                "  Markdown:EOL('\\n')\n" +
+                "  WHITE_SPACE(' ')\n" +
+                "  Markdown:SECTION\n" +
+                "    Markdown:SECTION_ID('$one')\n" +
+                "    Markdown::(':')\n" +
+                "    WHITE_SPACE(' ')\n" +
+                "    Markdown:PARAGRAPH\n" +
+                "      Markdown:TEXT('section one')",
+                getParsedTreeText("Summary\n" +
+                                  " $one: section one"));
+    }
+
     @Override
     protected String getTestDataPath() {
         return super.getTestDataPath() + "/kdoc";
