b9c0cbffee975d11a1d72115dcfc68bc9886892e
Valentin Fondaratov <fondarat@gmail.com>
2015-09-09 02:49:27 +0300

    gfm flavour descriptor

---
+++ src/org/intellij/markdown/flavours/gfm/GFMFlavourDescriptor.kt
@@ -0,0 +1,6 @@
+package org.intellij.markdown.flavours.gfm
+
+import org.intellij.markdown.flavours.commonmark.CommonMarkFlavourDescriptor
+
+public class GFMFlavourDescriptor : CommonMarkFlavourDescriptor() {
+}
