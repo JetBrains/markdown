a3403d5c6705f6de91cd16e55171a290d2b22b17
max.kammerer <max.kammerer@yahoo.com>
2015-12-28 14:24:58 +0300

    Added @JvmField annotation to object constants to access them from java

--- src/org/intellij/markdown/flavours/gfm/GFMElementTypes.kt
+++ src/org/intellij/markdown/flavours/gfm/GFMElementTypes.kt
@@ -5,14 +5,19 @@ import org.intellij.markdown.MarkdownElementType
 
 public object GFMTokenTypes {
 
+    @JvmField
     public val TILDE: IElementType = MarkdownElementType("~", true);
 
+    @JvmField
     public val TABLE_SEPARATOR: IElementType = MarkdownElementType("TABLE_SEPARATOR", true)
 
+    @JvmField
     public val GFM_AUTOLINK: IElementType = MarkdownElementType("GFM_AUTOLINK", true);
 
+    @JvmField
     public val CHECK_BOX: IElementType = MarkdownElementType("CHECK_BOX", true);
 
+    @JvmField
     public val CELL: IElementType = MarkdownElementType("CELL", true)
 }
 
