e328a4e9059ec2b052930c858ac8b2f61205efa8
Valentin Fondaratov <fondarat@gmail.com>
2019-10-23 14:58:15 +0300

    #31: prepend `http://` to GFM autolinks starting with `www.`
    It seems it's what github does to them

--- src/org/intellij/markdown/flavours/gfm/GFMFlavourDescriptor.kt
+++ src/org/intellij/markdown/flavours/gfm/GFMFlavourDescriptor.kt
@@ -54,8 +54,11 @@ open class GFMFlavourDescriptor : CommonMarkFlavourDescriptor() {
                 GFMTokenTypes.GFM_AUTOLINK to object : GeneratingProvider {
                     override fun processNode(visitor: HtmlGenerator.HtmlGeneratingVisitor, text: String, node: ASTNode) {
                         val linkText = node.getTextInNode(text)
+                        // according to GFM_AUTOLINK rule in lexer, link either starts with scheme or with 'www.'
+                        val linkDestination = if (linkText.startsWith("www.")) "http://$linkText" else linkText
+
                         val link = EntityConverter.replaceEntities(linkText, true, false)
-                        visitor.consumeTagOpen(node, "a", "href=\"${LinkMap.normalizeDestination(linkText, false)}\"")
+                        visitor.consumeTagOpen(node, "a", "href=\"${LinkMap.normalizeDestination(linkDestination, false)}\"")
                         visitor.consumeHtml(link)
                         visitor.consumeTagClose("a")
                     }
