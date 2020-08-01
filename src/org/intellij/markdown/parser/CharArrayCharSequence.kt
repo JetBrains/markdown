a7a2aa08e54cbfb2eb2f8079bbc426ce7d97a525
Valentin Fondaratov <fondarat@gmail.com>
2020-08-01 17:01:11 +0300

    cleanup CharArrayCharSequence, replace hashCode with own lazy

--- src/org/intellij/markdown/parser/CharArrayCharSequence.kt
+++ src/org/intellij/markdown/parser/CharArrayCharSequence.kt
@@ -4,6 +4,8 @@ class CharArrayCharSequence(private val chars: CharArray,
                             private val start: Int = 0,
                             override val length: Int = chars.size - start) : CharSequence {
 
+    private var hash: Int = 0
+
     override fun get(index: Int): Char = chars[start + index]
 
     override fun subSequence(startIndex: Int, endIndex: Int): CharSequence {
@@ -13,7 +15,7 @@ class CharArrayCharSequence(private val chars: CharArray,
         return CharArrayCharSequence(chars, start + startIndex, endIndex - startIndex)
     }
     
-    private fun checkIndex(index: Int) = index >= 0 && index <= length
+    private fun checkIndex(index: Int) = index in 0..length
 
     
     
@@ -28,7 +30,7 @@ class CharArrayCharSequence(private val chars: CharArray,
         other as CharArrayCharSequence
         
         if (other.length != length) return false
-        for (i in 0..length - 1) {
+        for (i in 0 until length) {
             if (get(i) != other.get(i)) {
                 return false
             }
@@ -37,15 +39,14 @@ class CharArrayCharSequence(private val chars: CharArray,
         return true
     }
 
-    private val hashCode: Int by lazy {
-        var h = 0
-        for (offset in start..start + length - 1) {
-            h = 31 * h + chars[offset].toInt()
-        }
-        return@lazy h
-    }
-
     override fun hashCode(): Int {
-        return hashCode
+        var h = hash
+        if (h == 0 && length > 0) {
+            for (offset in start until start + length) {
+                h = 31 * h + chars[offset].toInt()
+            }
+            hash = h
+        }
+        return h
     }
 }
