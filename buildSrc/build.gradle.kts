508ce9f416fdf6898fa14e9cfd102bffb671b089
Kamil Doległo <kamilok1965@interia.pl>
2020-12-21 12:05:41 +0100

    Adapt build files for MavenCentral release

---
+++ buildSrc/build.gradle.kts
@@ -0,0 +1,12 @@
+plugins {
+    `kotlin-dsl`
+}
+
+repositories {
+    mavenCentral()
+    jcenter()
+}
+
+dependencies {
+    implementation("com.jfrog.bintray.gradle:gradle-bintray-plugin:1.8.5")
+}
---
+++ buildSrc/src/main/kotlin/org/jetbrains/PublicationChannel.kt
@@ -0,0 +1,35 @@
+package org.jetbrains
+
+import org.gradle.api.Project
+
+internal enum class PublicationChannel {
+    Bintray,
+    MavenCentral,
+    MavenCentralSnapshot;
+
+    val isBintrayRepository
+        get() = this == Bintray
+
+    val isMavenRepository
+        get() = when (this) {
+            MavenCentral, MavenCentralSnapshot -> true
+            else -> false
+        }
+
+    companion object {
+        fun fromPropertyString(value: String) = when (value) {
+            "bintray" -> Bintray
+            "maven-central" -> MavenCentral
+            "maven-central-snapshot" -> MavenCentralSnapshot
+            else -> throw IllegalArgumentException("Unknown publication_channel=$value")
+        }
+    }
+}
+
+internal val Project.publicationChannels: Set<PublicationChannel>
+    get() = properties["publication_channels"]
+            ?.toString()
+            ?.split("&")
+            ?.map { channel -> PublicationChannel.fromPropertyString(channel) }
+            ?.toSet()
+            .orEmpty()
