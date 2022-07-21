bed5dc268721dad588173fd5ee700c88b0c19cb9
Ivan Posti <ivan.posti@jetbrains.com>
2022-07-21 18:57:19 +0200

    [#48] Allow creating tables with less than 3 dashes in delimiter row
    Some time after [#371](https://github.com/github/markup/issues/371) GFM relaxed constraints on the minimal number of required dashes in table delimiter row.

--- src/commonMain/kotlin/org/intellij/markdown/flavours/gfm/table/GitHubTableMarkerProvider.kt
+++ src/commonMain/kotlin/org/intellij/markdown/flavours/gfm/table/GitHubTableMarkerProvider.kt
@@ -71,7 +71,7 @@ class GitHubTableMarkerProvider : MarkerBlockProvider<MarkerProcessor.StateInfo>
                     dashes++
                 }
 
-                if (dashes < 3) {
+                if (dashes < 1) {
                     return 0
                 }
                 result++
