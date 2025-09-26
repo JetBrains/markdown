d0057f5f49cd54cba8029b9cd01ec1fe5bfa390a
spacedvoid <114347266+spacedvoid@users.noreply.github.com>
2025-09-26 06:45:28 +0300

    Don't include `[]` in a link label if on new line

--- src/commonMain/kotlin/org/intellij/markdown/parser/sequentialparsers/impl/ReferenceLinkParser.kt
+++ src/commonMain/kotlin/org/intellij/markdown/parser/sequentialparsers/impl/ReferenceLinkParser.kt
@@ -65,23 +65,20 @@ class ReferenceLinkParser : SequentialParser {
 
             val linkLabel = LinkParserUtil.parseLinkLabel(iterator)
                     ?: return null
-            
+
             var it = linkLabel.iteratorPosition
             val shortcutLinkEnd = it
 
             it = it.advance()
-            if (it.type == MarkdownTokenTypes.EOL) {
-                it = it.advance()
-            }
 
-            if (it.type == MarkdownTokenTypes.LBRACKET && it.rawLookup(1) == MarkdownTokenTypes.RBRACKET) {
+            if (it.start == shortcutLinkEnd.end && it.type == MarkdownTokenTypes.LBRACKET && it.rawLookup(1) == MarkdownTokenTypes.RBRACKET) {
                 it = it.advance()
             } else {
                 it = shortcutLinkEnd
             }
 
             return LocalParsingResult(it,
-                    linkLabel.parsedNodes 
+                    linkLabel.parsedNodes
                             + SequentialParser.Node(startIndex..it.index + 1, MarkdownElementTypes.SHORT_REFERENCE_LINK),
                     linkLabel.rangesToProcessFurther)
         }
