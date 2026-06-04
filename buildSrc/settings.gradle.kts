e9124d40f16c0f8fc6831b6232355c40a8bd0d07
Ilia Permiashkin <ilia.permiashkin@jetbrains.com>
2026-06-04 12:55:58 +0300

    Use cache redirector

--- buildSrc/settings.gradle.kts
+++ buildSrc/settings.gradle.kts
@@ -1,13 +1,6 @@
-plugins {
-    `kotlin-dsl`
-}
-
-repositories {
+pluginManagement {
+  repositories {
     maven("https://cache-redirector.jetbrains.com/plugins.gradle.org/m2")
     maven("https://cache-redirector.jetbrains.com/repo1.maven.org/maven2")
-    mavenCentral()
-}
-
-dependencies {
-    implementation("com.google.code.gson:gson:2.8.6")
+  }
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,6 +3,10 @@ rootProject.name = "markdown"
 include(":docs")
 
 pluginManagement {
+  repositories {
+    maven("https://cache-redirector.jetbrains.com/plugins.gradle.org/m2")
+    maven("https://cache-redirector.jetbrains.com/repo1.maven.org/maven2")
+  }
   plugins {
     kotlin("multiplatform") version "2.0.0" apply false
     id("org.jetbrains.dokka") version "1.8.20" apply false
