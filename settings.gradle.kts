01242a2280d594eef7f36a06ce79f56fc45c0017
Ilia Permiashkin <ilia.permiashkin@jetbrains.com>
2026-06-04 13:57:01 +0300

    Add missing repositories

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,6 +6,8 @@ pluginManagement {
   repositories {
     maven("https://cache-redirector.jetbrains.com/plugins.gradle.org/m2")
     maven("https://cache-redirector.jetbrains.com/repo1.maven.org/maven2")
+    gradlePluginPortal()
+    mavenCentral()
   }
   plugins {
     kotlin("multiplatform") version "2.0.0" apply false
